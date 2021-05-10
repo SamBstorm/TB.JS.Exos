8530fb2a51480aef806ba71738aaa9fd8b7796a6
Legrain Samuel <samuel.legrain@bstorm.be>
2021-05-10 11:24:31 +0200

    Exos Date - Add Date and style (for demonstration)

--- Exo - JS - Timer/scripts/js/main.js
+++ Exo - JS - Timer/scripts/js/main.js
@@ -2,8 +2,12 @@
 
 window.onload = function(){
     const body = document.body;
-    const h1 = document.createElement("h1");
-    body.appendChild(h1);
+    const h1_date = document.createElement("h1");
+    h1_date.setAttribute("id","date");
+    const h1_clock = document.createElement("h1");
+    h1_clock.setAttribute("id","clock");
+    body.appendChild(h1_date);
+    body.appendChild(h1_clock);
 
     const getTimer = function(cssSelector){
         let elemHTML = document.querySelector(cssSelector);
@@ -16,12 +20,41 @@ window.onload = function(){
             elemHTML.innerText = finalFormat;
         }
     };
+    
+    const getDate = function (cssSelector) {
+        const monthNames = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet","Août", "Septembre", "Octobre","Novembre", "Décembre"];
+        const dayNames = ["Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"];        
+        let elemHTML = document.querySelector(cssSelector);
+        if (elemHTML){
+            let date = new Date();
+            let dayOfWeek = date.getDay();
+            let month = date.getMonth();
+            let dayOfMonth = date.getDate();
+            let finalFormat = `${dayNames[dayOfWeek]} ${dayOfMonth} ${monthNames[month]}`;
+            elemHTML.innerText = finalFormat;
+        }
+    }
     getTimer("title");
     let title_timer = setInterval(
         function(){ getTimer("title"); }
         , 1000 );
-    getTimer("body>h1")
+    getTimer("#clock");
     let body_timer = setInterval(
-        function(){ getTimer("body>h1"); }
+        function(){ getTimer("#clock"); }
         , 1000 );
+    getDate("#date");
+
+    const millisecondPerDay = 86400000;
+    let date = new Date();
+    let ms = date.getMilliseconds() + (date.getSeconds()*1000) + (date.getMinutes()*60000) + (date.getHours() * 3600000);
+    let date_timer = setTimeout(
+        function(){
+            getDate("#date");
+            let t = setInterval(
+                getDate("#date"),
+                millisecondPerDay
+            )
+        }
+        ,millisecondPerDay - ms
+    );
 };
